b4c3dbd001cc3e9664b7a36e3cf1088361b43298
Nat Tuck <nat@ferrus.net>
2024-10-17 16:38:18 -0400

    minimal editor

--- app/page.js
+++ app/page.js
@@ -4,12 +4,17 @@ import Image from "next/image";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import Editor from './editor';
 
 export default function Home() {
   return (
     <Row>
       <Col>
-        <p>Home Page</p>
+        <h1>Code</h1>
+        <Editor />
+      </Col>
+      <Col>
+        <h1>Output</h1>
       </Col>
     </Row>
   );
